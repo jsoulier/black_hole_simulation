c9231dae6ca5b5fa8073e6ad0cc74a64b83888cf
Jaan Soulier <jaansoulier@gmail.com>
2025-09-15 19:29:26 -0400

    all major tests passing

--- test/savepoint.cpp
+++ test/savepoint.cpp
@@ -121,7 +121,6 @@ struct EntityV3
     float Y = 2.0f;
     ItemV2 Item;
     int Health = 3;
-    int Intelligence = 5;
 
     void Visit(SavepointArchive& archive)
     {
@@ -130,7 +129,6 @@ struct EntityV3
         archive(Y);
         archive(Item, kVersion2);
         archive(Health);
-        archive(Intelligence, kVersion3);
     }
 
     bool operator==(const EntityV1& other) const
@@ -139,8 +137,7 @@ struct EntityV3
             X == other.X &&
             Y == other.Y &&
             Item == ItemV2{} &&
-            Health == other.Health &&
-            Intelligence == 5;
+            Health == other.Health;
     }
 
     bool operator==(const EntityV2& other) const
@@ -149,8 +146,7 @@ struct EntityV3
             X == other.X &&
             Y == other.Y &&
             Item == other.Item &&
-            Health == other.Health &&
-            Intelligence == 5;
+            Health == other.Health;
     }
 
     bool operator==(const EntityV3& other) const
@@ -159,8 +155,7 @@ struct EntityV3
             X == other.X &&
             Y == other.Y &&
             Item == other.Item &&
-            Health == other.Health &&
-            Intelligence == other.Intelligence;
+            Health == other.Health;
     }
 };
 
@@ -172,7 +167,6 @@ struct EntityV4
     float Y = 2.0f;
     ItemV4 Item;
     int Health = 3;
-    int Intelligence = 5;
 
     void Visit(SavepointArchive& archive)
     {
@@ -182,7 +176,6 @@ struct EntityV4
         archive(Z, kVersion4);
         archive(Item, kVersion2);
         archive(Health);
-        archive(Intelligence, kVersion3);
     }
 
     bool operator==(const EntityV1& other) const
@@ -192,8 +185,7 @@ struct EntityV4
             Z == 6.0f &&
             Y == other.Y &&
             Item == ItemV4{} &&
-            Health == other.Health &&
-            Intelligence == 5;
+            Health == other.Health;
     }
 
     bool operator==(const EntityV2& other) const
@@ -203,8 +195,7 @@ struct EntityV4
             Z == 6.0f &&
             Y == other.Y &&
             Item == other.Item &&
-            Health == other.Health &&
-            Intelligence == 5;
+            Health == other.Health;
     }
 
     bool operator==(const EntityV3& other) const
@@ -214,8 +205,7 @@ struct EntityV4
             Z == 6.0f &&
             Y == other.Y &&
             Item == other.Item &&
-            Health == other.Health &&
-            Intelligence == 5;
+            Health == other.Health;
     }
 
     bool operator==(const EntityV4& other) const
@@ -225,8 +215,7 @@ struct EntityV4
             Z == other.Z &&
             Y == other.Y &&
             Item == other.Item &&
-            Health == other.Health &&
-            Intelligence == other.Intelligence;
+            Health == other.Health;
     }
 };
 
@@ -239,22 +228,15 @@ struct ZombieV1 : EntityV1
         EntityV1::Visit(archive);
         archive(Speed);
     }
-};
 
-struct ZombieV21 : EntityV1
-{
-    float Speed = 1.0f;
-    float VelocityX = 2.0f;
-
-    void Visit(SavepointArchive& archive)
+    bool operator==(const ZombieV1& other) const
     {
-        EntityV1::Visit(archive);
-        archive(Speed);
-        archive(VelocityX, kVersion2);
+        return EntityV1::operator==(other) &&
+            Speed == other.Speed;
     }
 };
 
-struct ZombieV22 : EntityV2
+struct ZombieV2 : EntityV2
 {
     float Speed = 1.0f;
     float VelocityX = 2.0f;
@@ -265,27 +247,121 @@ struct ZombieV22 : EntityV2
         archive(Speed);
         archive(VelocityX, kVersion2);
     }
+
+    bool operator==(const ZombieV1& other) const
+    {
+        return EntityV2::operator==(other) &&
+            Speed == other.Speed &&
+            VelocityX == 2.0f;
+    }
+
+    bool operator==(const ZombieV2& other) const
+    {
+        return EntityV2::operator==(other) &&
+            Speed == other.Speed &&
+            VelocityX == other.VelocityX;
+    }
+};
+
+struct ZombieV3 : EntityV3
+{
+    float Speed = 1.0f;
+    float VelocityX = 2.0f;
+    float VelocityY = 3.0f;
+    float VelocityZ = 4.0f;
+
+    void Visit(SavepointArchive& archive)
+    {
+        EntityV3::Visit(archive);
+        archive(Speed);
+        archive(VelocityX, kVersion2);
+        archive(VelocityY, kVersion3);
+        archive(VelocityZ, kVersion3);
+    }
+
+    bool operator==(const ZombieV1& other) const
+    {
+        return EntityV3::operator==(other) &&
+            Speed == other.Speed &&
+            VelocityX == 2.0f &&
+            VelocityY == 3.0f &&
+            VelocityZ == 4.0f;
+    }
+
+    bool operator==(const ZombieV2& other) const
+    {
+        return EntityV3::operator==(other) &&
+            Speed == other.Speed &&
+            VelocityX == other.VelocityX &&
+            VelocityY == 3.0f &&
+            VelocityZ == 4.0f;
+    }
+
+    bool operator==(const ZombieV3& other) const
+    {
+        return EntityV3::operator==(other) &&
+            Speed == other.Speed &&
+            VelocityX == other.VelocityX &&
+            VelocityY == other.VelocityY &&
+            VelocityZ == other.VelocityZ;
+    }
 };
 
 struct ZombieV4 : EntityV4
 {
     float Speed = 1.0f;
     float VelocityX = 2.0f;
-    float VelocityY = 2.0f;
-    float VelocityZ = 2.0f;
+    float VelocityY = 3.0f;
+    float VelocityZ = 4.0f;
 
     void Visit(SavepointArchive& archive)
     {
         EntityV4::Visit(archive);
         archive(Speed);
         archive(VelocityX, kVersion2);
-        archive(VelocityY, kVersion4);
-        archive(VelocityZ, kVersion4);
+        archive(VelocityY, kVersion3);
+        archive(VelocityZ, kVersion3);
+    }
+
+    bool operator==(const ZombieV1& other) const
+    {
+        return EntityV4::operator==(other) &&
+            Speed == other.Speed &&
+            VelocityX == 2.0f &&
+            VelocityY == 3.0f &&
+            VelocityZ == 4.0f;
+    }
+
+    bool operator==(const ZombieV2& other) const
+    {
+        return EntityV4::operator==(other) &&
+            Speed == other.Speed &&
+            VelocityX == other.VelocityX &&
+            VelocityY == 3.0f &&
+            VelocityZ == 4.0f;
+    }
+
+    bool operator==(const ZombieV3& other) const
+    {
+        return EntityV4::operator==(other) &&
+            Speed == other.Speed &&
+            VelocityX == other.VelocityX &&
+            VelocityY == other.VelocityY &&
+            VelocityZ == other.VelocityZ;
+    }
+
+    bool operator==(const ZombieV4& other) const
+    {
+        return EntityV4::operator==(other) &&
+            Speed == other.Speed &&
+            VelocityX == other.VelocityX &&
+            VelocityY == other.VelocityY &&
+            VelocityZ == other.VelocityZ;
     }
 };
 
 template<typename InT, typename OutT>
-void TestUpgrade(Savepoint& savepoint, SavepointVersion inVersion)
+void Test(Savepoint& savepoint, SavepointVersion inVersion)
 {
     SavepointArchive inArchive{inVersion};
     SavepointID inEntityID;
@@ -311,16 +387,31 @@ int main()
     std::filesystem::remove(kFileName + "-journal");
     Savepoint savepoint;
     assert(savepoint.Open(kFileName));
-    TestUpgrade<EntityV1, EntityV1>(savepoint, kVersion1);
-    TestUpgrade<EntityV1, EntityV2>(savepoint, kVersion1);
-    TestUpgrade<EntityV1, EntityV3>(savepoint, kVersion1);
-    TestUpgrade<EntityV1, EntityV4>(savepoint, kVersion1);
-    TestUpgrade<EntityV2, EntityV2>(savepoint, kVersion2);
-    TestUpgrade<EntityV2, EntityV3>(savepoint, kVersion2);
-    TestUpgrade<EntityV2, EntityV4>(savepoint, kVersion2);
-    TestUpgrade<EntityV3, EntityV3>(savepoint, kVersion3);
-    TestUpgrade<EntityV3, EntityV4>(savepoint, kVersion3);
-    TestUpgrade<EntityV4, EntityV4>(savepoint, kVersion4);
+
+    // basic upgrade tests
+    Test<EntityV1, EntityV1>(savepoint, kVersion1);
+    Test<EntityV1, EntityV2>(savepoint, kVersion1);
+    Test<EntityV1, EntityV3>(savepoint, kVersion1);
+    Test<EntityV1, EntityV4>(savepoint, kVersion1);
+    Test<EntityV2, EntityV2>(savepoint, kVersion2);
+    Test<EntityV2, EntityV3>(savepoint, kVersion2);
+    Test<EntityV2, EntityV4>(savepoint, kVersion2);
+    Test<EntityV3, EntityV3>(savepoint, kVersion3);
+    Test<EntityV3, EntityV4>(savepoint, kVersion3);
+    Test<EntityV4, EntityV4>(savepoint, kVersion4);
+
+    // inheritance upgrade tests
+    Test<ZombieV1, ZombieV1>(savepoint, kVersion1);
+    Test<ZombieV1, ZombieV2>(savepoint, kVersion1);
+    Test<ZombieV2, ZombieV2>(savepoint, kVersion2);
+    Test<ZombieV1, ZombieV3>(savepoint, kVersion1);
+    Test<ZombieV2, ZombieV3>(savepoint, kVersion2);
+    Test<ZombieV3, ZombieV3>(savepoint, kVersion3);
+    Test<ZombieV1, ZombieV4>(savepoint, kVersion1);
+    Test<ZombieV2, ZombieV4>(savepoint, kVersion2);
+    Test<ZombieV3, ZombieV4>(savepoint, kVersion3);
+    Test<ZombieV4, ZombieV4>(savepoint, kVersion4);
+
     savepoint.Save();
     savepoint.Close();
     return 0;
