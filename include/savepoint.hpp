67a030e45176dd8c7ac61b735e984b6080de7763
Jaan Soulier <jaansoulier@gmail.com>
2025-10-11 20:13:54 -0400

    add some docstrings

--- include/savepoint.hpp
+++ include/savepoint.hpp
@@ -15,73 +15,164 @@
 #include <utility>
 #include <vector>
 
+/**
+ * @brief The signature of the log function
+ * 
+ * @param string The message
+ */
 using SavepointLogFunction = std::function<void(const std::string_view& string)>;
 
+/**
+ * @brief Replace the default log function
+ * 
+ * @param function 
+ */
 void SavepointSetLogFunction(const SavepointLogFunction& function);
+
+/**
+ * @brief Log to stderr
+ * 
+ * @param string 
+ */
 void SavepointDefaultLogFunction(const std::string_view& string);
+
+/**
+ * @brief Forward a message to the set log function
+ * 
+ * @param string 
+ */
 void SavepointLog(const std::string_view& string);
 
+/**
+ * @brief Representation of a major.minor.patch version
+ */
 class SavepointVersion
 {
 private:
     friend class Savepoint;
 
 public:
+    /**
+     * @brief Create a version with the smallest value
+     */
     constexpr SavepointVersion()
         : Value{0}
     {
     }
 
+    /**
+     * @brief Create a version
+     * 
+     * @param major The major version
+     * @param minor The minor version
+     * @param patch The patch version
+     */
     constexpr SavepointVersion(uint32_t major, uint32_t minor, uint32_t patch)
         : Value{major << 24 | minor << 16 | patch}
     {
     }
 
+    /**
+     * @brief Get the major version
+     * 
+     * @return The major version
+     */
     constexpr uint32_t GetMajor() const
     {
         return (Value >> 24) & 0xFF;
     }
 
+    /**
+     * @brief Get the minor version
+     * 
+     * @return The minor version
+     */
     constexpr uint32_t GetMinor() const
     {
         return (Value >> 16) & 0xFF;
     }
 
+    /**
+     * @brief Get the patch version
+     * 
+     * @return The patch version
+     */
     constexpr uint32_t GetPatch() const
     {
         return Value & 0xFFFF;
     }
 
+    /**
+     * @brief Get the version as a string with the format major.minor.patch
+     * 
+     * @return The version string
+     */
     std::string GetString() const
     {
         return std::format("{}.{}.{}", GetMajor(), GetMinor(), GetPatch());
     }
 
+    /**
+     * @brief Check if the version is equal to the other
+     * 
+     * @param other The other version
+     * @return If the version is equal
+     */
     constexpr bool operator==(const SavepointVersion other) const
     {
         return Value == other.Value;
     }
 
+    /**
+     * @brief Check if the version is not equal to the other
+     * 
+     * @param other The other version
+     * @return If the version is not equal
+     */
     constexpr bool operator!=(const SavepointVersion other) const
     {
         return Value != other.Value;
     }
 
+    /**
+     * @brief Check if the version is less than to the other
+     * 
+     * @param other The other version
+     * @return If the version is less than
+     */
     constexpr bool operator<(const SavepointVersion other) const
     {
         return Value < other.Value;
     }
 
+    /**
+     * @brief Check if the version is greater than to the other
+     * 
+     * @param other The other version
+     * @return If the version is greater than
+     */
     constexpr bool operator>(const SavepointVersion other) const
     {
         return Value > other.Value;
     }
 
+    /**
+     * @brief Check if the version is less than or equal to the other
+     * 
+     * @param other The other version
+     * @return If the version is less than or equal
+     */
     constexpr bool operator<=(const SavepointVersion other) const
     {
         return Value <= other.Value;
     }
 
+    /**
+     * @brief Check if the version is greater than or equal to the other
+     * 
+     * @param other The other version
+     * @return If the version is greater than or equal
+     */
     constexpr bool operator>=(const SavepointVersion other) const
     {
         return Value >= other.Value;
@@ -91,27 +182,52 @@ private:
     uint32_t Value;
 };
 
+/**
+ * Unique ID for referencing entities (not unique across different savepoints)
+ */
 class SavepointID
 {
 private:
     friend class Savepoint;
 
 public:
+    /**
+     * @brief Create an invalid ID
+     */
     constexpr SavepointID()
         : Value{std::numeric_limits<uint32_t>::max()}
     {
     }
 
+    /**
+     * @brief Check if the ID is equal to the other
+     * 
+     * @param other The other ID
+     * 
+     * @return If the ID is equal
+     */
     constexpr bool operator==(const SavepointID other) const
     {
         return Value == other.Value;
     }
 
+    /**
+     * @brief Check if the ID is not equal to the other
+     * 
+     * @param other The other ID
+     * 
+     * @return If the ID is not equal
+     */
     constexpr bool operator!=(const SavepointID other) const
     {
         return Value != other.Value;
     }
 
+    /**
+     * @brief Check if the ID is valid
+     * 
+     * @return If the ID is valid
+     */
     constexpr operator bool() const
     {
         return Value != SavepointID{}.Value;
@@ -121,24 +237,51 @@ private:
     uint32_t Value;
 };
 
+/**
+ * @brief Check if a type is a pointer
+ * 
+ * @tparam The type
+ */
 template<typename T>
 struct SavepointPointerImpl : std::is_pointer<T> {};
 
+/**
+ * @copydoc SavepointPointerImpl
+ */
 template<typename T>
 struct SavepointPointerImpl<std::shared_ptr<T>> : std::true_type {};
 
+/**
+ * @copydoc SavepointPointerImpl
+ */
 template<typename T, typename Deleter>
 struct SavepointPointerImpl<std::unique_ptr<T, Deleter>> : std::true_type {};
 
+/**
+ * @copydoc SavepointPointerImpl
+ */
 template<typename T>
 inline constexpr bool SavepointPointer = SavepointPointerImpl<T>::value;
 
+/**
+ * @brief Check if a type is visitable
+ * 
+ * @tparam The type
+ */
 template<typename T>
 concept SavepointVisitable = requires { { &T::Visit }; };
 
+/**
+ * @brief Check if a type is a primitive (can be copied)
+ * 
+ * @tparam The type
+ */
 template<typename T>
 concept SavepointPrimitive = !SavepointPointer<T> && !SavepointVisitable<T>;
 
+/**
+ * Visitor for serializing to/from a byte stream
+ */
 class SavepointVisitor
 {
 private:
@@ -149,6 +292,11 @@ private:
     SavepointVisitor& operator=(const SavepointVisitor& other) = delete;
 
 public:
+    /**
+     * @brief Create a new visitor
+     * 
+     * @param version The version (should be the version of your application)
+     */
     SavepointVisitor(SavepointVersion version)
         : Version{version}
         , Writer{}
@@ -158,15 +306,15 @@ public:
         operator()(version);
     }
 
-    SavepointVisitor& operator=(SavepointVisitor&& other)
-    {
-        Version = other.Version;
-        Writer = std::move(other.Writer);
-        Reader = other.Reader;
-        Offset = other.Offset;
-        return *this;
-    }
-
+    /**
+     * @brief Serialize to/from the byte stream
+     * 
+     * @tparam T The type to serialize
+     * @tparam Args The types of the arguments for construction
+     * @param item The item to serialize
+     * @param version The visitor version required to deserialize
+     * @param args The args to forward for construction if deserialization requirements aren't met
+     */
     template<SavepointPrimitive T, typename... Args>
     void operator()(T& item, SavepointVersion version = {}, Args&&... args)
     {
@@ -182,7 +330,7 @@ public:
             }
             if (Offset + sizeof(T) > Reader.size())
             {
-                SavepointLog(std::format("Tried to read past the end of an visitor: {} -> {}", Version.GetString(), version.GetString()));
+                SavepointLog(std::format("Tried to read past visitor: {} -> {}", Version.GetString(), version.GetString()));
                 if constexpr (sizeof...(Args) > 0)
                 {
                     item = T{std::forward<Args>(args)...};
@@ -199,6 +347,9 @@ public:
         }
     }
 
+    /**
+     * @copydoc operator()
+     */
     template<SavepointVisitable T, typename... Args>
     void operator()(T& item, SavepointVersion version = {}, Args&&... args)
     {
@@ -216,11 +367,12 @@ public:
         item.Visit(*this);
     }
 
+    /**
+     * @brief Reset the visitor
+     */
     void Reset()
     {
-        Reader = {};
-        Writer.clear();
-        operator()(Version);
+        Writer.resize(sizeof(Version));
     }
 
 private:
@@ -237,38 +389,147 @@ private:
     uint32_t Offset;
 };
 
+/**
+ * @brief The signature of the reader callback
+ */
 using SavepointFunction = std::function<void(SavepointVisitor& visitor)>;
+
+/**
+ * @brief The signature of the entity reader callback
+ */
 using SavepointEntityFunction = std::function<void(SavepointVisitor& visitor, SavepointID id)>;
+
+/**
+ * @brief The signature of the 2D tile reader callback
+ */
 using SavepointTile2DFunction = std::function<void(SavepointVisitor& visitor, int x, int y)>;
+
+/**
+ * @brief The signature of the 3D tile reader callback
+ */
 using SavepointTile3DFunction = std::function<void(SavepointVisitor& visitor, int x, int y, int z)>;
 
+/**
+ * @brief Return codes
+ */
 enum class SavepointStatus
 {
-    Failed,
-    Existing,
-    New,
+    Failed,   /**< A failure occured */
+    Existing, /**< An existing savepoint was opened */
+    New,      /**< A new savepoint was opened */
 };
 
+/**
+ * Database connection handle
+ */
 class Savepoint
 {
 public:
+    /**
+     * Create a new savepoint
+     */
     Savepoint();
     Savepoint(const Savepoint& other) = delete;
     Savepoint& operator=(const Savepoint& other) = delete;
     Savepoint(Savepoint&& other) = delete;
     Savepoint& operator=(Savepoint&& other) = delete;
+    
+    /**
+     * @brief Open the savepoint
+     * 
+     * @param path The path to the file
+     * @return The savepoint status
+     */
     SavepointStatus Open(const std::string_view& path);
+
+    /**
+     * @brief Close the savepoint (does NOT save!)
+     */
     void Close();
+    
+    /**
+     * @brief Save pending changes
+     */
     void Save();
+
+    /**
+     * @brief Write a global visitor (not shared between savepoints)
+     * 
+     * @param visitor The visitor to write
+     */
     void Write(const SavepointVisitor& visitor);
+
+    /**
+     * @brief Write a visitor referenced by an ID
+     * 
+     * @param visitor The visitor to write
+     * @param id The ID to use (not unique between levels). If invalid, writes to id
+     * @param level The level to write. If id exists, moves to the new level
+     */
     void Write(const SavepointVisitor& visitor, SavepointID& id, int level);
+
+    /**
+     * @brief Write a visitor to a 2D tile location
+     * 
+     * @param visitor The visitor to write
+     * @param x The x coordinate
+     * @param y The y coordinate
+     * @param level The level to write
+     */
     void Write(const SavepointVisitor& visitor, int x, int y, int level);
+
+    /**
+     * @brief Write a visitor to a 2D tile location
+     * 
+     * @param visitor The visitor to write
+     * @param x The x coordinate
+     * @param y The y coordinate
+     * @param z The z coordinate
+     * @param level The level to write
+     */
     void Write(const SavepointVisitor& visitor, int x, int y, int z, int level);
+
+    /**
+     * @brief Read a global visitor (not shared between savepoints)
+     * 
+     * @param function The callback to use
+     */
     void Read(const SavepointFunction& function);
+
+    /**
+     * @brief Read visitors referenced by an ID
+     * 
+     * @param function The callback to use
+     * @param level The level to read
+     */
     void Read(const SavepointEntityFunction& function, int level);
+
+    /**
+     * @brief Read visitors referenced by 2D coordinates
+     * 
+     * @param function The callback to use
+     * @param level The level to read
+     */
     void Read(const SavepointTile2DFunction& function, int level);
+
+    /**
+     * @brief Read visitors referenced by 3D coordinates
+     * 
+     * @param function The callback to use
+     * @param level The level to read
+     */
     void Read(const SavepointTile3DFunction& function, int level);
+
+    /**
+     * @brief Delete an ID
+     * 
+     * @param id The ID to delete
+     */
     void Delete(const SavepointID id);
+
+    /**
+     * @brief Delete all entities and tiles
+     */
     void Clear();
 
 private:
