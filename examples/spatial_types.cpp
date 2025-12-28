90ac9c2400647a4f42afca6666f690982732d92b
Jaan Soulier <jaansoulier@gmail.com>
2025-12-27 22:04:02 -0500

    attributes

--- examples/spatial_types.cpp
+++ examples/spatial_types.cpp
@@ -38,7 +38,7 @@ int main()
     // Optional
     std::random_device device;
     std::mt19937 generator(device());
-    std::uniform_int_distribution<std::mt19937::result_type> distribution(0,2);
+    std::uniform_int_distribution<std::mt19937::result_type> distribution(0, 2);
 
     // Writing tiles
     SavepointVisitor inVisitor;
